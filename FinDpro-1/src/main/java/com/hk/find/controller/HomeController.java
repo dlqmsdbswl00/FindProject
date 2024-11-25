62e5882d5a02ab09a0c79ea4641f978b8b31161a
김윤지 <21615008@konyang.ac.kr>
2024-11-25 10:11:53 +0900

    마이페이지 작성 중

--- FinDpro-1/src/main/java/com/hk/find/controller/HomeController.java
+++ FinDpro-1/src/main/java/com/hk/find/controller/HomeController.java
@@ -3,19 +3,20 @@ package com.hk.find.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        // 사용자 정보를 모델에 추가 (임시 데이터)
-        model.addAttribute("userName", "아무개");
-        model.addAttribute("accountBalance", "10,000,000 원");
-        model.addAttribute("monthlyExpense", "180,000 원");
-        model.addAttribute("mainSpending", "식비 40%");
-        model.addAttribute("habitSaving", "1,000 원");
+	@GetMapping("/")
+	public String home(Model model) {
+		// 사용자 정보를 모델에 추가 (임시 데이터)
+		model.addAttribute("userName", "임시 사용자");
+		model.addAttribute("accountBalance", "10,000,000 원");
+		model.addAttribute("monthlyExpense", "180,000 원");
+		model.addAttribute("mainSpending", "식비 40%");
+		model.addAttribute("habitSaving", "1,000 원");
 
-        return "home"; // home.html 템플릿으로 이동
-    }
+		return "home"; // home.html 템플릿으로 이동
+	}
 }
---
+++ FinDpro-1/src/main/java/com/hk/find/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.hk.find.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    // 로그인 페이지
+    @GetMapping("/User/login")
+    public String loginPage() {
+        return "User/login"; // login.html 템플릿 반환
+    }
+
+    // 회원가입 페이지
+    @GetMapping("/User/register")
+    public String registerPage() {
+        return "User/register"; // register.html 템플릿 반환
+    }
+
+    // 회원가입 처리
+    @PostMapping("/User/register")
+    public String registerUser(Model model) {
+        // TODO: 회원가입 처리 로직 (DB 저장 등)
+        model.addAttribute("message", "회원가입 성공!");
+        return "redirect:/User/login"; // 회원가입 후 로그인 페이지로 리다이렉트
+    }
+
+    // 마이페이지
+    @GetMapping("/User/mypage")
+    public String myPage(Model model /*, @AuthenticationPrincipal CustomUserDetails userDetails */) {
+        // TODO: 로그인한 사용자 정보 가져오는 로직
+        // 임시 데이터로 사용자 이름 전달
+        model.addAttribute("userName", "임시 사용자");
+        return "User/mypage"; // mypage.html 템플릿 반환
+    }
+}
